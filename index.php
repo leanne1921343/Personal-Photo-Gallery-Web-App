73711f8fd6618429c6a5f349769506f5918becab
leanne1921343 <76035533+leanne1921343@users.noreply.github.com>
2020-12-15 16:37:16 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html>
+<style>
+.header {
+	background-color: #62BFB6;
+	color: #ffffff;
+	padding: 0px;
+	width: 800px;
+	height: 40px;
+	margin-left: auto;
+	margin-right: auto;
+	text-align: center;
+	font-family: century gothic;
+}
+.elem{
+	text-align: center;
+}
+.body {
+  background-color: #000000;
+}
+.center {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  width: 50%;
+.column {
+  margin-top: 2px;
+  vertical-align: middle;
+  width: 60%;
+}
+}
+.row {
+  display: flex;
+  flex-wrap: wrap;
+  padding: 0 4px;
+}
+.a{
+	color: #ffffff
+	margin-left: auto;
+	margin-right: auto;
+	text-align: center;
+	font-family: century gothic;
+}
+</style>
+<body class="body">
+	<div class="header">
+		<h1 class="elem">ELEMENTARY</h1>
+	</div>
+	<div class="center">
+	<div class="column">
+		<img src="45388_500207626702335_162634952_n.jpg" width="200" height="200">
+		<img src="215432_500057290050702_634310950_n.jpg" width="200" height="200">
+		<img src="223133_3942794288495_1760407427_n.jpg" width="200" height="200">
+		<img src="375900_3943016894060_1773863761_n.jpg" width="200" height="200">
+		<img src="577008_500207570035674_2025203327_n.jpg" width="200" height="200">
+	</div>
+	</div>
+	<div class="header">
+		<h1 class="elem">JUNIOR HIGH SCHOOL</h1>
+	</div>
+	<div class="center">
+	<div class="column">
+		<img src="11051880_1013086042081155_6401658990165173099_o.jpg" width="200" height="200">
+		<img src="13582026_1315973448431700_6655159053936636209_o.jpg" width="200" height="200">
+		<img src="17634527_10209389310989936_603834232790591156_n.jpg" width="200" height="200">
+		<img src="17760828_1366368733419549_8141812907758392225_o.jpg" width="200" height="200">
+		<img src="22089630_1532250626831358_3474081876968087808_n.jpg" width="200" height="200">
+	</div>
+	</div>
+	<div class="header">
+		<h1 class="elem">SENIOR HIGH SCHOOL</h1>
+	</div>
+	<div class="center">
+	<div class="column">
+		<img src="36926099_1049140888588590_587579958776299520_n.jpg" width="200" height="200">
+		<img src="42672448_10212485626032699_8670104232503803904_o.jpg" width="200" height="200">
+		<img src="42686651_10212485636592963_6159324148637106176_o.jpg" width="200" height="200">
+		<img src="42761247_10212485359186028_5845516220035497984_o.jpg" width="200" height="200">
+		<img src="59168536_10214891130171977_4848417992943337472_n.jpg" width="200" height="200">
+	</div>
+	</div>
+	<div class="header">
+		<h1 class="elem">COLLEGE</h1>
+	</div>
+	<div class="center">
+	<div class="column">
+		<img src="131141068_149431116592811_4914369388770077880_n.jpg" width="200" height="200">
+		<img src="131225374_738391543511707_2042526674421889756_n.jpg" width="200" height="200">
+		<img src="131270143_229631305196915_1470140893449489310_n.jpg" width="200" height="200">
+		<img src="131442521_135966064961956_202173544387532164_n.jpg" width="200" height="200">
+		<img src="131515638_874290406675321_7988855535694404205_n.jpg" width="200" height="200">
+	</div>
+	</div>
+	<a href="hoami.html" class="a"><center>BACK to HOME</center></a>
+</body>
+</html>
